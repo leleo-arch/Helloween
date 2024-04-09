ef6640c429d2cb6c8e0798d2d5c10a991f7dcbdc
leleo-arch <leo_xdplay@hotmail.com>
2024-04-09 18:00:02 -0300

    first-commit

---
+++ src/containers/Home/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    Conteiner,
+    Imagem,
+    ConteinerItens,
+    H1,
+    Button,
+    User,
+  } from "./Style";
+
+  import logo from "../../assets/Logo.png"
+  import Fantasma from "../../assets/fantasma.png"
+
+  function LP (){
+
+    <Conteiner>
+     <Imagem alt="img-Logo" src={logo}>
+     </Imagem>
+     <ConteinerItens>
+    <H1>Page-Halloween</H1>
+    <Button>
+    </Button>
+    <Button>
+    </Button>
+    <User></User>
+    </ConteinerItens>
+    <Imagem alt="img-Logo" src={Fantasma}>
+     </Imagem>
+    </Conteiner>
+    
+  }
+
+  export default LP
